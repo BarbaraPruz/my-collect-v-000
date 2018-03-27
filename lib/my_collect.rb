5f351f922755caf0a2e0ebe2bb6d08bb26799c30
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-27 19:30:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,10 @@ def collect (array)
   if block_given?
     i = 0
     while i<array.size do
-      collection << yield (array[i])
+      collection << yield array[i]
       i += 1
     end
   end
   collection
 end
+
